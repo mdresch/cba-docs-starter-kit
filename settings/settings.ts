74f9bb8cb7d61beb1ddfd90986b33bee451ffeb8
Menno Drescher <menno@cbaconsult.eu>
2025-02-15 20:27:47 +0100

    Setttings page fully completed

--- settings/settings.ts
+++ settings/settings.ts
@@ -1,25 +1,25 @@
-export const url = "https://rubix-documents.vercel.app"
+export const url = "https://cba-docs-starter-kit.vercel.app/"
 export const siteicon = "/icon.png"
 
 export const gtm = "GTM-XXXXXXX"
 export const gtmconnected = true
 
-export const sitename = "Documents"
+export const sitename = "CBA Docs"
 export const description =
   "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements."
 export const keywords = [
   "Next.js",
   "documentation",
-  "Rubix Studios",
+  "CBA Studios",
   "open source",
   "SEO",
 ]
 export const urlimage = "/images/og-image.png"
-export const imagealt = "Rubix Studios Documentation"
-export const twitterhandle = "@RubixStudios"
+export const imagealt = "CBA Studios Documentation"
+export const twitterhandle = "@consult_cba"
 
-export const companyname = "Rubix Studios"
-export const companylink = "https://www.rubixstudios.com.au"
+export const companyname = "CBA Studios"
+export const companylink = "https://www.google.com"
 
 export const branding = true
 export const rightsidebar = true
@@ -27,4 +27,4 @@ export const feedbackedit = true
 export const tableofcontent = true
 export const totopscroll = true
 
-export const loadfromgithub = false
+export const loadfromgithub = true
